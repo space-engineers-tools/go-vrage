1e19586dcb08c9bd1e6ef99ebfad878528ad42af
EchterTimo <EchterTimo@users.noreply.github.com>
2026-08-01 01:12:48 +0200

    edit doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,4 @@
 // Package vrage provides a client for interacting with the Space Engineers VRage Remote API.
+//
+// Before using this package, ensure that the <RemoteApiEnabled> setting is set to true in SpaceEngineers-Dedicated.cfg and that the target server is running.
 package vrage
