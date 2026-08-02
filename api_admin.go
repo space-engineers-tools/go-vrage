21d5916aef11c1df28200b7eb0d42c3f85c4cdfa
EchterTimo <EchterTimo@users.noreply.github.com>
2026-08-02 03:17:53 +0200

    add admin and session api structs

---
+++ api_admin.go
@@ -0,0 +1,8 @@
+package vrage
+
+// APIAdmin provides access to the /v1/admin API routes.
+type APIAdmin struct {
+	http *HTTPClient
+}
+
+// todo: APIAdmin needs methods
---
+++ api_session.go
@@ -0,0 +1,8 @@
+package vrage
+
+// APISession provides access to the /v1/session API routes.
+type APISession struct {
+	http *HTTPClient
+}
+
+// todo: APISession needs methods
