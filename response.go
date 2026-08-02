eaf473e57733a54494d7b74066853f856ec5ae01
EchterTimo <EchterTimo@users.noreply.github.com>
2026-08-02 03:11:29 +0200

    define base response structure

---
+++ response.go
@@ -0,0 +1,13 @@
+package vrage
+
+// BaseResponse represents the base response structure of the API.
+type BaseResponse struct {
+	Data JSON `json:"data"`
+	Meta Meta `json:"meta"`
+}
+
+// Meta represents the metadata of the API response.
+type Meta struct {
+	APIVersion string  `json:"apiVersion"`
+	QueryTime  float64 `json:"queryTime"`
+}
