11fed8d542b4e80846b0fe915c6cea07b20acd98
EchterTimo <EchterTimo@users.noreply.github.com>
2026-08-02 06:50:06 +0200

    add example for ping cmd

---
+++ examples/ping/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/space-engineers-tools/go-vrage"
+)
+
+func panicIfError(err error) {
+	if nil != err {
+		panic(err)
+	}
+}
+
+func main() {
+	v, err := vrage.NewClient(vrage.ClientConfig{
+		UseHTTPS:          false,
+		RemoteApiIP:       "127.0.0.1",
+		RemoteApiPort:     11202,
+		RemoteSecurityKey: "PLEASE USE A STRONG SECURITY KEY",
+	})
+	panicIfError(err)
+
+	ping, err := v.Server.PingV1()
+	panicIfError(err)
+
+	fmt.Println("The server uses api version:", ping.Meta.APIVersion)
+	fmt.Println(ping.Data.Result)
+	fmt.Println(ping.Meta.QueryTime)
+}
