4689125adc6ab27337472ff5e81a930e13c19c11
EchterTimo <EchterTimo@users.noreply.github.com>
2026-08-05 17:46:48 +0200

    set default timeout to 5 seconds

--- config.go
+++ config.go
@@ -9,7 +9,7 @@ import (
 
 // Default configuration values for the VRage Remote API client.
 const (
-	DefaultTimeout      time.Duration = 10 * time.Second
+	DefaultTimeout      time.Duration = 5 * time.Second
 	DefaultBaseEndpoint string        = "/vrageremote"
 	DefaultPort         uint32        = 8080
 )
