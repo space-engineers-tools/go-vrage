723c83fb5efeeaf77abaab631ba46644f15b1b5d
EchterTimo <EchterTimo@users.noreply.github.com>
2026-08-09 22:29:37 +0200

    steam user is uint64; implement ban endpoints

--- api_admin.go
+++ api_admin.go
@@ -10,7 +10,7 @@ type APIAdmin struct {
 // region /v1/admin/promotedPlayers
 
 // PromotePlayer promotes a player with the specified Steam ID.
-func (a *APIAdmin) PromotePlayer(steamID string) (APIResponseWithoutData, error) {
+func (a *APIAdmin) PromotePlayer(steamID uint64) (APIResponseWithoutData, error) {
 	var responseStruct APIResponseWithoutData
 	httpResponse, err := a.http.PostV1AdminPromotedPlayersSteamID(steamID)
 	if err != nil {
@@ -31,9 +31,82 @@ func (a *APIAdmin) PromotePlayer(steamID string) (APIResponseWithoutData, error)
 }
 
 // DemotePlayer demotes a player with the specified Steam ID.
-func (a *APIAdmin) DemotePlayer(steamID string) (APIResponseWithoutData, error) {
+func (a *APIAdmin) DemotePlayer(steamID uint64) (APIResponseWithoutData, error) {
 	var responseStruct APIResponseWithoutData
 	httpResponse, err := a.http.DeleteV1AdminPromotedPlayersSteamID(steamID)
+	if err != nil {
+		return responseStruct, err
+	}
+	defer func() {
+		if err := httpResponse.Body.Close(); err != nil {
+			log.Printf("failed to close response body: %v", err)
+		}
+	}()
+
+	responseStruct, err = parseResponse[APIResponseWithoutData](httpResponse)
+	if err != nil {
+		return responseStruct, err
+	}
+
+	return responseStruct, nil
+}
+
+// region /v1/admin/bannedPlayers
+
+// APIAdminBannedPlayersData represents the data returned by the GET /v1/admin/bannedPlayers endpoint.
+type APIAdminBannedPlayersData struct {
+	BannedPlayers []struct {
+		SteamID     uint64 `json:"SteamId"`
+		DisplayName string `json:"DisplayName"` // DisplayName can be empty
+	} `json:"BannedPlayers"`
+}
+
+// BannedPlayers fetches a list of banned players.
+func (a *APIAdmin) BannedPlayers() (APIResponseWithData[APIAdminBannedPlayersData], error) {
+	var responseStruct APIResponseWithData[APIAdminBannedPlayersData]
+	httpResponse, err := a.http.GetV1AdminBannedPlayers()
+	if err != nil {
+		return responseStruct, err
+	}
+	defer func() {
+		if err := httpResponse.Body.Close(); err != nil {
+			log.Printf("failed to close response body: %v", err)
+		}
+	}()
+
+	responseStruct, err = parseResponse[APIResponseWithData[APIAdminBannedPlayersData]](httpResponse)
+	if err != nil {
+		return responseStruct, err
+	}
+
+	return responseStruct, nil
+}
+
+// BanPlayer bans a player with the specified Steam ID.
+func (a *APIAdmin) BanPlayer(steamID uint64) (APIResponseWithoutData, error) {
+	var responseStruct APIResponseWithoutData
+	httpResponse, err := a.http.PostV1AdminBannedPlayersSteamID(steamID)
+	if err != nil {
+		return responseStruct, err
+	}
+	defer func() {
+		if err := httpResponse.Body.Close(); err != nil {
+			log.Printf("failed to close response body: %v", err)
+		}
+	}()
+
+	responseStruct, err = parseResponse[APIResponseWithoutData](httpResponse)
+	if err != nil {
+		return responseStruct, err
+	}
+
+	return responseStruct, nil
+}
+
+// UnbanPlayer unbans a player with the specified Steam ID.
+func (a *APIAdmin) UnbanPlayer(steamID uint64) (APIResponseWithoutData, error) {
+	var responseStruct APIResponseWithoutData
+	httpResponse, err := a.http.DeleteV1AdminBannedPlayersSteamID(steamID)
 	if err != nil {
 		return responseStruct, err
 	}
--- http_admin.go
+++ http_admin.go
@@ -1,25 +1,48 @@
 package vrage
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 // region /v1/admin/promotedPlayers
 
 // PostV1AdminPromotedPlayersSteamID promotes a player with the specified Steam ID and returns the HTTP response.
 //
 //	POST /v1/admin/promotedPlayers/{steam_id}
-func (c *HTTPClient) PostV1AdminPromotedPlayersSteamID(steamID string) (*http.Response, error) {
-	return c.DoErr(http.MethodPost, "/v1/admin/promotedPlayers/"+steamID, jsonMap(nil), httpHeaders(nil))
+func (c *HTTPClient) PostV1AdminPromotedPlayersSteamID(steamID uint64) (*http.Response, error) {
+	return c.DoErr(http.MethodPost, fmt.Sprintf("/v1/admin/promotedPlayers/%d", steamID), jsonMap(nil), httpHeaders(nil))
 }
 
 // DeleteV1AdminPromotedPlayersSteamID demotes a player with the specified Steam ID and returns the HTTP response.
 //
 //	DELETE /v1/admin/promotedPlayers/{steam_id}
-func (c *HTTPClient) DeleteV1AdminPromotedPlayersSteamID(steamID string) (*http.Response, error) {
-	return c.DoErr(http.MethodDelete, "/v1/admin/promotedPlayers/"+steamID, jsonMap(nil), httpHeaders(nil))
+func (c *HTTPClient) DeleteV1AdminPromotedPlayersSteamID(steamID uint64) (*http.Response, error) {
+	return c.DoErr(http.MethodDelete, fmt.Sprintf("/v1/admin/promotedPlayers/%d", steamID), jsonMap(nil), httpHeaders(nil))
 }
 
 // region /v1/admin/bannedPlayers
-// todo: v1/admin/bannedPlayers endpoints
+
+// GetV1AdminBannedPlayers fetches a list of banned players and returns the HTTP response.
+//
+//	GET /v1/admin/bannedPlayers
+func (c *HTTPClient) GetV1AdminBannedPlayers() (*http.Response, error) {
+	return c.DoErr(http.MethodGet, "/v1/admin/bannedPlayers", jsonMap(nil), httpHeaders(nil))
+}
+
+// PostV1AdminBannedPlayersSteamID bans a player with the specified Steam ID and returns the HTTP response.
+//
+//	POST /v1/admin/bannedPlayers/{steam_id}
+func (c *HTTPClient) PostV1AdminBannedPlayersSteamID(steamID uint64) (*http.Response, error) {
+	return c.DoErr(http.MethodPost, fmt.Sprintf("/v1/admin/bannedPlayers/%d", steamID), jsonMap(nil), httpHeaders(nil))
+}
+
+// DeleteV1AdminBannedPlayersSteamID unbans a player with the specified Steam ID and returns the HTTP response.
+//
+//	DELETE /v1/admin/bannedPlayers/{steam_id}
+func (c *HTTPClient) DeleteV1AdminBannedPlayersSteamID(steamID uint64) (*http.Response, error) {
+	return c.DoErr(http.MethodDelete, fmt.Sprintf("/v1/admin/bannedPlayers/%d", steamID), jsonMap(nil), httpHeaders(nil))
+}
 
 // region /v1/admin/kickedPlayers
 // todo: v1/admin/kickedPlayers endpoints
